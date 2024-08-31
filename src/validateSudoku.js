9e45b68966ba0b6f843a8055788c075108d7664e
Thiago Martins <133309154+ThiagoJMartins@users.noreply.github.com>
2024-08-31 16:08:20 -0300

    Update validateSudoku.js

--- src/validateSudoku.js
+++ src/validateSudoku.js
@@ -45,28 +45,6 @@
 
 function isValidSudoku(board) {
 	// Escriba su ejercicio aquí
-	const rows = new Array(9).fill().map(() => new Set());
-	const cols = new Array(9).fill().map(() => new Set());
-	const boxes = new Array(9).fill().map(() => new Set());
-
-	for (let i = 0; i < 9; i++) {
-		for (let j = 0; j < 9; j++) {
-			const num = board[i][j];
-			if (num === ".") continue;
-
-			const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-
-			if (rows[i].has(num) || cols[j].has(num) || boxes[boxIndex].has(num)) {
-				return false;
-			}
-
-			rows[i].add(num);
-			cols[j].add(num);
-			boxes[boxIndex].add(num);
-		}
-	}
-
-	return true;
 }
 
 module.exports = isValidSudoku;
